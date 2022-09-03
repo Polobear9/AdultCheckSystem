34c7169c6be43b4cb70d448b010e7fbc480b75f5
Polobear9 <lieams1996@gmail.com>
2022-09-03 23:47:18 +0900

    Adjust System and Code more be Simpled.

--- src/Test/CheckTheAdult.java
+++ src/Test/CheckTheAdult.java
@@ -3,53 +3,62 @@ package Test;
 import java.time.LocalDateTime;
 
 public class CheckTheAdult {
-	
-	private String Adult;
-	
-	public void Check(InfoUser user) {
+
+    private String Adult;
+    private String result;
+
+    public String getResult() {
+        return result;
+    }
+
+    public void setResult(String result) {
+        this.result = result;
+    }
+
+    public void Check(InfoUser user) {
 		//it is change the type userBirth int - > String
-//		String userBirth = String.valueOf(user.getBirthday());		
+//		String userBirth = String.valueOf(user.getBirthday());
 		//userBirth 0~4 = Year / 4~6 = Month / 6~8 = Day
-		if(user.getBirthday() != null) {
-		String userYear = user.getBirthday().substring(0, 4);
-		String userMonth = user.getBirthday().substring(4, 6);
-		String userDay = user.getBirthday().substring(6, 8);	
-		//Change the Birth Info type to int from String
-		int TheUserYear = Integer.parseInt(userYear);
-		int TheUserMonth = Integer.parseInt(userMonth);
-		int TheUserDay = Integer.parseInt(userDay);
-		int YearAge = LocalDateTime.now().getYear() - TheUserYear;
-		//Chack the Age, user is Adult? or Not.
-		if(YearAge >= 20) {
-			if(YearAge == 20) {
-				int CheckMonth = TheUserMonth - LocalDateTime.now().getMonthValue();
-					if(CheckMonth == 0) {
-						int CheckDay = TheUserDay - LocalDateTime.now().getDayOfMonth();
-							if(CheckDay <= 0) {
-								String result = "You are Adult.";
-								this.Adult = result;
-							}else {
-								String result = "You are not Adult";
-								this.Adult = result;
-							}
-					}else if(CheckMonth < 0) {
-						String result = "You are Adult.";
-						this.Adult = result;
-					}else {
-						String result = "You are not Adult";
-						this.Adult = result;
-					}
-				
-				}else if(YearAge > 20){
-					String result = "You are Adult";
-					this.Adult = result;
-				}else {
-					String result = "You are Not Adult";
-					this.Adult = result;
-				}
-		}
-		//Tell User, User is a Adult ? ot Not.
-		System.out.println(Adult);
-	}
-}
+		if (user.getBirthday() != null) {
+			String userYear = user.getBirthday().substring(0, 4);
+			String userMonth = user.getBirthday().substring(4, 6);
+			String userDay = user.getBirthday().substring(6, 8);
+			int theUserYear = Integer.parseInt(userYear);
+			int theUserMonth = Integer.parseInt(userMonth);
+			int theUserDay = Integer.parseInt(userDay);
+			int yearAge = LocalDateTime.now().getYear() - theUserYear;
+			//Change the Birth Info type to int from String
+			//Check the Age, user is Adult? or Not.
+			if (yearAge >= 20) {
+                if (yearAge == 20) {
+                    int CheckMonth = theUserMonth - LocalDateTime.now().getMonthValue();
+                    if (CheckMonth == 0) {
+                        int CheckDay = theUserDay - LocalDateTime.now().getDayOfMonth();
+                        if (CheckDay <= 0) {
+                            result = "You are Adult.";
+                            this.setResult(result);
+                        } else {
+                            result = "You are not Adult";
+                            this.setResult(result);
+                        }
+                    } else if (CheckMonth < 0) {
+                        result = "You are Adult.";
+                        this.setResult(result);
+                    } else {
+                        result = "You are not Adult";
+                        this.setResult(result);
+                    }
+
+                } else if (yearAge > 20) {
+                    result = "You are Adult";
+                    this.setResult(result);
+                } else {
+                    result = "You are Not Adult";
+                    this.setResult(result);
+                }
+            }
+            //Tell User, User is a Adult ? ot Not.
+            System.out.println(Adult);
+        }
+    }
 }
